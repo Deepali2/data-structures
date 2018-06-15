418a460a55180770eefcf89df29edaf28698d89d
Deepali2 <hdgarg@gmail.com>
2018-06-15 10:09:47 -0700

    implements linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var newNode = Node(value);
+    if(list.head === null) {
+      list.head = newNode;
+      list.tail = newNode;  
+    }
+    list.tail.next = newNode;//pointing but not filling
+    list.tail = newNode;//filling
   };
 
   list.removeHead = function() {
+    var currentHead = list.head;
+    list.head = list.head.next;
+    return currentHead.value;
   };
 
   list.contains = function(target) {
+    var currentNode = list.head;
+    while(currentNode) {
+      if (currentNode.value === target) {
+        return true;
+      }
+      currentNode = currentNode.next;
+    }
+      return false;
   };
 
   return list;
