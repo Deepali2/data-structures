e6a3c5cee871397fe01c50eb66d88d1d55d45312
Deepali2 <hdgarg@gmail.com>
2018-06-16 15:16:21 -0700

    Changed binarySearchTree from ES6 to prototypal

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,61 +1,109 @@
-//var BinarySearchTree = function(value) {
-//};
+var BinarySearchTree = function(value) {
 
-class Node {
-  constructor(){
-    this.left = left;
-    this.right = right;
-    this.value = value;
-  }
-}
+  var binaryTree = Object.create(binaryTreePrototype);
+  binaryTree.value = value;
+  binaryTree.left = null;
+  binaryTree.right = null;
+  return binaryTree;
+  };
 
-class BinarySearchTree {
-  constructor(){
-    this.root = null; //starting point of tree
-  } 
+var binaryTreePrototype = {};
 
-insertNode(node, newNode){
-  if(newNode.value > node.value){
-    if(!node.right){
-      node.right = newNode;
+binaryTreePrototype.insert = function(val) {
+  if (val < this.value) {
+    if (this.left === null) {
+      this.left = BinarySearchTree(val);
+    } else {
+      this.left.insert(val);
     }
-    this.insertNode(node.right, newNode);  
-  }
-    if(!node.left){
-      node.left = newNode;
+  } else if (val > this.value) {
+    if (this.right === null) {
+      this.right = BinarySearchTree(val);
+    } else {
+      this.right.insert(val);
     }
-    this.insertNode(ndoe.left, newNode);
-}
-
-insert(value){
-  var newNode = new Node(value);
-  if(this.root === null){
-    this.root = newNode;
-  }  
-  this.insertNode(this.root, newNode);
-}
-
-contains(value, node = this.node){
-  if(!node){
-    return false; 
+  } else {
+    // do nothing: The tree already contains this value
   }
-  if(value === node.value){
+};
+
+binaryTreePrototype.contains = function(val) {
+  if (val === this.value) {
     return true;
+  } else if (val < this.value) {
+    return !!(this.left && this.left.contains(val));
+  } else if (val > this.value) {
+    return !!(this.right && this.right.contains(val));
   }
-  if(value > node.value){
-    return this.contains(value, node.right);    
-  }
-  if(value < node.value){
-    return this.contains(value, node.left);
-  }
-  return false;
-}
+};
 
-// depthFirstLog(cb, node = this.node){
-  
+binaryTreePrototype.depthFirstLog = function(callBack) {
+  callBack(this.value);
+  if (this.left) {
+    this.left.depthFirstLog(callBack);
+  }
+  if (this.right) {
+    this.right.depthFirstLog(callBack);
+  }
+};
+
+// class Node {
+//   constructor(value){
+//     this.left = null;
+//     this.right = null;
+//     this.value = value;
+//   }
 // }
 
-}
+// class BinarySearchTree {
+//   constructor(value){
+//     this.root = null; //starting point of tree
+//   } 
+
+// insertNode(node, newNode){
+//   if(newNode.value > node.value){
+//     if(node.right === null){
+//       node.right = newNode;
+//     }
+//     node.insertNode(node.right, newNode);  
+//   }
+//   if(newNode.value < node.value){
+//     if(node.left === null){
+//       node.left = newNode;
+//     }
+//     node.insertNode(node.left, newNode);
+//   }
+// }
+
+// insert(value){
+//   var newNode = new Node(value);
+//   if(this.root === null){
+//     this.root = newNode;
+//   }  
+//   this.insertNode(this.root, newNode);
+// }
+
+// contains(value, node = this.node){
+//   if(!node){
+//     return false; 
+//   }
+//   if(value === node.value){
+//     return true;
+//   }
+//   if(value > node.value){
+//     return this.contains(value, node.right);    
+//   }
+//   if(value < node.value){
+//     return this.contains(value, node.left);
+//   }
+//   return false;
+// }
+
+// // depthFirstLog(cb, node = this.node){
+  
+// // }
+
+// }
 
 
 
