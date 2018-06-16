09a89567b692d2f274495537d9b320578ab440f6
Deepali2 <hdgarg@gmail.com>
2018-06-16 10:48:14 -0700

    inserts node, contains, working on parenthesis

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,5 +1,62 @@
-var BinarySearchTree = function(value) {
-};
+//var BinarySearchTree = function(value) {
+//};
+
+class BinarySearchTree {
+  constructor(){
+    this.root = null; //starting point of tree
+  } 
+}
+
+class Node {
+  constructor(){
+    this.left = left;
+    this.right = right;
+    this.value = value;
+  }
+}
+
+insertNode(node, newNode){
+  if(newNode.value > node.value){
+    if(!node.right){
+      node.right = newNode;
+    }
+    this.insertNode(node.right, newNode);  
+  }
+    if(!node.left){
+      node.left = newNode;
+    }
+    this.insertNode(ndoe.left, newNode);
+}
+
+insert(value){
+  var newNode = new Node(value);
+  if(this.root === null){
+    this.root = newNode;
+  }  
+  this.insertNode(this.root, newNode);
+}
+
+contains(value, node = this.node){
+  if(!node){
+    return false; 
+  }
+  if(value === node.value){
+    return true;
+  }
+  if(value > node.value){
+    return this.contains(value, node.right);    
+  }
+  if(value < node.value){
+    return this.contains(value, node.left);
+  }
+  return false;
+}
+
+depthFirstLog(cb, node = this.node){
+  
+}
+
+
 
 
 /*
